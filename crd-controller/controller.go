ed43f72a17cd4ca10027a8de359b142592c7ef2e
James.Y.Yang (tech.cncd02.Newegg) 42035 <james.yang516@outlook.com>
2023-04-04 20:42:02 +0800

    update controller

--- crd-controller/controller.go
+++ crd-controller/controller.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 
+	emptyappv1alpha1 "k8s-client-demo/crd-controller/apis/emptyapp/v1alpha1"
+	clientset "k8s-client-demo/crd-controller/client/clientset/versioned"
+	informers "k8s-client-demo/crd-controller/client/informers/externalversions/emptyapp/v1alpha1"
 	customlisters "k8s-client-demo/crd-controller/client/listers/emptyapp/v1alpha1"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+
+	appsinformers "k8s.io/client-go/informers/apps/v1"
+	appslisters "k8s.io/client-go/listers/apps/v1"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"k8s.io/apimachinery/pkg/api/errors"
 
 	"k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
@@ -14,18 +29,58 @@ import (
 )
 
 type Controller struct {
-	lister   customlisters.EmptyAppLister
-	queue    workqueue.RateLimitingInterface
-	informer cache.Controller
+	kubeclientset  kubernetes.Interface
+	emptyclientset clientset.Interface
+
+	emptyLister customlisters.EmptyAppLister
+	emptySynced cache.InformerSynced
+
+	deploymentsLister appslisters.DeploymentLister
+	deploymentsSynced cache.InformerSynced
+
+	queue workqueue.RateLimitingInterface
+	// recorder record.EventRecorder
 }
 
-func NewController(queue workqueue.RateLimitingInterface,
-	lister customlisters.EmptyAppLister, informer cache.Controller) *Controller {
-	return &Controller{
-		informer: informer,
-		lister:   lister,
-		queue:    queue,
+func NewController(
+	kubeclientset kubernetes.Interface,
+	sampleclientset clientset.Interface,
+	deploymentInformer appsinformers.DeploymentInformer,
+	emptyInformer informers.EmptyAppInformer) *Controller {
+	controller := &Controller{
+		kubeclientset:     kubeclientset,
+		emptyclientset:    sampleclientset,
+		deploymentsLister: deploymentInformer.Lister(),
+		deploymentsSynced: deploymentInformer.Informer().HasSynced,
+		emptyLister:       emptyInformer.Lister(),
+		emptySynced:       emptyInformer.Informer().HasSynced,
+		queue:             workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter()),
 	}
+
+	emptyAppHandler := cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			key, err := cache.MetaNamespaceKeyFunc(obj)
+			if err == nil {
+				controller.queue.Add(key)
+			}
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			key, err := cache.MetaNamespaceKeyFunc(newObj)
+			if err == nil {
+				controller.queue.Add(key)
+			}
+		},
+		DeleteFunc: func(obj interface{}) {
+			key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+			if err == nil {
+				controller.queue.Add(key)
+			}
+		},
+	}
+
+	emptyInformer.Informer().AddEventHandler(emptyAppHandler)
+
+	return controller
 }
 
 func (c *Controller) Run(workers int, stopCh chan struct{}) {
@@ -35,10 +90,10 @@ func (c *Controller) Run(workers int, stopCh chan struct{}) {
 	defer c.queue.ShutDown()
 	klog.Info("Starting Deployment controller")
 
-	go c.informer.Run(stopCh)
+	// go c.informer.Run(stopCh)
 
 	// Wait for all involved caches to be synced, before processing items from the queue is started
-	if !cache.WaitForCacheSync(stopCh, c.informer.HasSynced) {
+	if !cache.WaitForCacheSync(stopCh, c.deploymentsSynced, c.emptySynced) {
 		runtime.HandleError(fmt.Errorf("Timed out waiting for caches to sync"))
 		return
 	}
@@ -85,7 +140,60 @@ func (c *Controller) procesResource(key string) error {
 	}
 
 	// Get the Foo resource with this namespace/name
-	app, err := c.lister.EmptyApps(namespace).Get(name)
+	app, err := c.emptyLister.EmptyApps(namespace).Get(name)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			runtime.HandleError(fmt.Errorf("EmptyApp '%s' in work queue no longer exists", key))
+			return nil
+		}
+		return err
+	}
+
+	imageName := app.Spec.ImageName
+	if imageName == "" {
+		// We choose to absorb the error here as the worker would requeue the
+		// resource otherwise. Instead, the next time the resource is updated
+		// the resource will be queued again.
+		runtime.HandleError(fmt.Errorf("%s: image name must be specified", key))
+		return nil
+	}
+
+	deploymentName := fmt.Sprintf("%s-empty-deployment", app.Name)
+
+	deployment, err := c.deploymentsLister.Deployments(app.Namespace).Get(deploymentName)
+	// If the resource doesn't exist, we'll create it
+	if errors.IsNotFound(err) {
+
+		deployment, err = c.kubeclientset.AppsV1().Deployments(app.Namespace).
+			Create(context.TODO(), newDeployment(app, deploymentName), metav1.CreateOptions{})
+	}
+
+	// If an error occurs during Get/Create, we'll requeue the item so we can
+	// attempt processing again later. This could have been caused by a
+	// temporary network failure, or any other transient reason.
+	if err != nil {
+		return err
+	}
+
+	// If this number of the replicas on the Foo resource is specified, and the
+	// number does not equal the current desired replicas on the Deployment, we
+	// should update the Deployment resource.
+	if (app.Spec.Replicas != 0 && app.Spec.Replicas != *deployment.Spec.Replicas) ||
+		(app.Spec.ImageName != deployment.Spec.Template.Spec.Containers[0].Image) {
+		deployment, err = c.kubeclientset.AppsV1().Deployments(app.Namespace).
+			Update(context.TODO(), newDeployment(app, deploymentName), metav1.UpdateOptions{})
+	}
+
+	// If an error occurs during Update, we'll requeue the item so we can
+	// attempt processing again later. This could have been caused by a
+	// temporary network failure, or any other transient reason.
+	if err != nil {
+		return err
+	}
+
+	// Finally, we update the status block of the EmptyApp resource to reflect the
+	// current state of the world
+	err = c.updateEmptyAppStatus(app, deployment)
 	if err != nil {
 		return err
 	}
@@ -95,6 +203,21 @@ func (c *Controller) procesResource(key string) error {
 	fmt.Printf("Sync/Add/Update for EmptyApp %s, Replicas: %d\n", app.GetName(), app.Spec.Replicas)
 
 	return nil
+}
+
+func (c *Controller) updateEmptyAppStatus(app *emptyappv1alpha1.EmptyApp, deployment *appsv1.Deployment) error {
+	// NEVER modify objects from the store. It's a read-only, local cache.
+	// You can use DeepCopy() to make a deep copy of original object and modify this copy
+	// Or create a copy manually for better performance
+	appCopy := app.DeepCopy()
+	appCopy.Status.AvailableReplicas = deployment.Status.AvailableReplicas
+	// If the CustomResourceSubresources feature gate is not enabled,
+	// we must use Update instead of UpdateStatus to update the Status block of the Foo resource.
+	// UpdateStatus will not allow changes to the Spec of the resource,
+	// which is ideal for ensuring nothing other than resource status has been updated.
+	_, err := c.emptyclientset.CrdV1alpha1().EmptyApps(app.Namespace).
+		UpdateStatus(context.TODO(), appCopy, metav1.UpdateOptions{})
+	return err
 }
 
 func (c *Controller) handleErr(err error, key interface{}) {
@@ -120,4 +243,42 @@ func (c *Controller) handleErr(err error, key interface{}) {
 	// Report to an external entity that, even after several retries, we could not successfully process this key
 	runtime.HandleError(err)
 	klog.Infof("Dropping deployment %q out of the queue: %v", key, err)
+}
+
+// newDeployment creates a new Deployment for a EmptyApp resource. It also sets
+// the appropriate OwnerReferences on the resource so handleObject can discover
+// the EmptyApp resource that 'owns' it.
+func newDeployment(app *emptyappv1alpha1.EmptyApp, deploymentName string) *appsv1.Deployment {
+	labels := map[string]string{
+		"app":        "emptyapp",
+		"controller": app.Name,
+	}
+	return &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      deploymentName,
+			Namespace: app.Namespace,
+			OwnerReferences: []metav1.OwnerReference{
+				*metav1.NewControllerRef(app, emptyappv1alpha1.SchemeGroupVersion.WithKind("EmptyApp")),
+			},
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &app.Spec.Replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: labels,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: labels,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "empty-server",
+							Image: app.Spec.ImageName,
+						},
+					},
+				},
+			},
+		},
+	}
 }
--- crd-controller/main.go
+++ crd-controller/main.go
@@ -12,12 +12,13 @@ import (
 	customscheme "k8s-client-demo/crd-controller/client/clientset/versioned/scheme"
 	custominformers "k8s-client-demo/crd-controller/client/informers/externalversions"
 
+	kubeinformers "k8s.io/client-go/informers"
+
 	"k8s.io/client-go/kubernetes/scheme"
 
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/workqueue"
 	"k8s.io/klog"
 )
 
@@ -36,45 +37,29 @@ func main() {
 		klog.Fatal(err)
 	}
 
+	kubeClient, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		klog.Fatal(err)
+	}
+
 	client, err := customclient.NewForConfig(config)
 	if err != nil {
 		klog.Fatal(err)
 	}
 
+	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeClient, time.Second*30)
 	factory := custominformers.NewSharedInformerFactory(client, time.Hour)
-	informer := factory.Crd().V1alpha1().EmptyApps()
-	queue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
 
-	deployHandler := cache.ResourceEventHandlerFuncs{
-		AddFunc: func(obj interface{}) {
-			key, err := cache.MetaNamespaceKeyFunc(obj)
-			if err == nil {
-				queue.Add(key)
-			}
-		},
-		UpdateFunc: func(oldObj, newObj interface{}) {
-			key, err := cache.MetaNamespaceKeyFunc(newObj)
-			if err == nil {
-				queue.Add(key)
-			}
-		},
-		DeleteFunc: func(obj interface{}) {
-			key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
-			if err == nil {
-				queue.Add(key)
-			}
-		},
-	}
-
-	informer.Informer().AddEventHandler(deployHandler)
-
-	ctrl := NewController(queue, informer.Lister(), informer.Informer())
+	ctrl := NewController(kubeClient, client,
+		kubeInformerFactory.Apps().V1().Deployments(),
+		factory.Crd().V1alpha1().EmptyApps())
 
 	stopper := make(chan os.Signal, 1)
 	signal.Notify(stopper, os.Interrupt, syscall.SIGTERM)
 	defer close(stopper)
 
 	stop := make(chan struct{})
+	kubeInformerFactory.Start(stop)
 	factory.Start(stop)
 	go ctrl.Run(1, stop)
 
